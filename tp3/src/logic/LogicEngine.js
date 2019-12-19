158df19bb67d8b244dcf9150d3579cb6ea40ecdc
João Nuno Matos <joao.nuno.matos@fe.up.pt>
2019-12-19 11:22:43 +0000

    Moved stuff around

---
+++ tp3/src/logic/LogicEngine.js
@@ -0,0 +1,17 @@
+class LogicEngine {
+    async getInitialState(width, height) {
+        throw "Not Implemented";
+    }
+
+    async isValidMove(gameState, move) {
+        throw "Not Implemented";
+    }
+
+    async makeMove(gameState, move) {
+        throw "Not Implemented";
+    }
+
+    async gameOver(gameState) {
+        throw "Not Implemented";
+    }
+}
